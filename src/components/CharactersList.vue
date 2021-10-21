2e1f0ccbcbd1df0c7414c0cea31ed8ff2cf2babb
mguer <marianne.guerrero42@laposte.net>
2021-10-21 23:48:04 +0200

    feat: img thumbnail

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,32 +1,49 @@
 <template>
   <v-row class="justify-center">
     <v-card
-      v-for="character in characters"
-      :key="character.id"
+      v-for="(character, index) in characters"
+      :key="character.id+character.name"
       col="3"
       class="ma-5"
       max-width="344"
     >
       <v-img
-        src="https://blog.comic-con-paris.com/wp-content/uploads/2019/07/super-heros-marvel-min.jpg"
+        :src="character.thumbnail.path ? `${character.thumbnail.path}/${imgSize}` : defaultImg"
         class="white--text align-end"
         gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
         height="200px"
       >
-        <v-card-title >{{ character.name }}</v-card-title>
+        <v-card-title>{{ character.name }}</v-card-title>
       </v-img>
 
       <v-card-actions>
-        <v-spacer></v-spacer>
-
-        <v-btn icon>
+        <v-btn text>
           <v-icon>fas fa-heart</v-icon>
         </v-btn>
 
-        <v-btn icon>
-          <v-icon>mdi-bookmark</v-icon>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          icon
+          @click="showInfo(index)"
+        >
+          <v-icon>fas fa-heart</v-icon>
         </v-btn>
       </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="index === selectedIndex ">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he
+            could deliver. You won't have time for sleeping, soldier, not with
+            all the bed making you'll be doing. Then we'll go with that data
+            file! Hey, you add a one and two zeros to that or we walk! You're
+            going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
     </v-card>
   </v-row>
 </template>
@@ -42,7 +59,9 @@ export default {
     ts: "?ts=1519211809934",
     publicKey: "&apikey=a9d50df97a5f822a7edcd9afbe4e357a",
     hash: "&hash=a8add32b487ad7a6951689990256b007",
+    imgSize: "standard_large.jpg",
     characters: [],
+    selectedIndex : null,
     selectedHeroes: [],
     defaultImg:
       "https://blog.comic-con-paris.com/wp-content/uploads/2019/07/super-heros-marvel-min.jpg",
@@ -57,10 +76,16 @@ export default {
             this.characters.push(item);
           });
         });
+      console.log(this.characters)
     },
+    showInfo(index){
+    console.log(this.selectedIndex)
+    console.log(index)
+    }
   },
   created() {
     this.fetchData();
+
   },
 };
 </script>
