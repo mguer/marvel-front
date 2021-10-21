8cca323e11ef697c307ef97b7df291186ca1004c
mguer <marianne.guerrero42@laposte.net>
2021-10-22 00:49:07 +0200

    feat: add heros to bookmark & add limit & offset

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,15 +1,28 @@
 <template>
   <v-row class="justify-center">
+    <v-card col="3" class="ma-5" width="350">
+      <v-card-title>Mes personnages préférés</v-card-title>
+      <v-list-item>
+        <v-list-item-content>
+          <p>{{ bookmarkHeroes }}</p>
+          <!--          <v-list-item-title v-for="hero in bookmarkHeroes">{{ hero }}</v-list-item-title>-->
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+
     <v-card
       v-for="(character, index) in characters"
-      :key="character.id+character.name"
+      :key="character.id + character.name"
       col="3"
       class="ma-5"
       width="350"
     >
       <v-img
-
-        :src="character.thumbnail.path ? `${character.thumbnail.path}/${imgSize}` : defaultImg"
+        :src="
+          character.thumbnail.path
+            ? `${character.thumbnail.path}/${imgSize}`
+            : defaultImg
+        "
         class="white--text align-end"
         gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
       >
@@ -17,31 +30,37 @@
       </v-img>
 
       <v-card-actions>
-        <v-btn text>
+        <v-btn text @click="addToBookmark(character.name)">
           <v-icon>fas fa-heart</v-icon>
         </v-btn>
 
         <v-spacer></v-spacer>
 
-        <v-btn
-          icon
-          @click="showInfo(index)"
-        >
-          <v-icon>{{ index === selectedIndex ? 'fas fa-arrow-up' : 'fas fa-arrow-down' }}</v-icon>
+        <v-btn icon @click="showInfo(index)">
+          <v-icon>{{
+            index === selectedIndex ? "fas fa-arrow-up" : "fas fa-arrow-down"
+          }}</v-icon>
         </v-btn>
       </v-card-actions>
 
       <v-expand-transition>
-        <div v-show="index === selectedIndex ">
+        <div v-show="index === selectedIndex">
           <v-divider></v-divider>
 
           <v-card-text>
-            {{ character.description ? character.description : "pas de description"}}
+            {{
+              character.description
+                ? character.description
+                : "pas de description"
+            }}
           </v-card-text>
           <v-card-text>
             nombre de comics : {{ character.comics.items.length }}
           </v-card-text>
-          <v-card-text v-for=" (comics,index)  in character.comics.items.slice(0,3) " :key="comics.name">
+          <v-card-text
+            v-for="(comics, index) in character.comics.items.slice(0, 3)"
+            :key="comics.name"
+          >
             {{ index + 1 }} : {{ comics.name }}
           </v-card-text>
         </div>
@@ -58,44 +77,52 @@ export default {
   data: () => ({
     //url params
     url: "http://gateway.marvel.com/v1/public/characters",
-    offset: "?offset=100",
-    ts: "?ts=1519211809934",
+    limit: "?limit=20",
+    offset: "&offset=100",
+    ts: "&ts=1519211809934",
     publicKey: "&apikey=a9d50df97a5f822a7edcd9afbe4e357a",
     hash: "&hash=a8add32b487ad7a6951689990256b007",
     imgSize: "standard_large.jpg",
     //layouts options
     characters: [],
-    selectedIndex : null,
-    selectedHeroes: [],
+    selectedIndex: null,
+    bookmarkHeroes: [],
     defaultImg:
       "https://blog.comic-con-paris.com/wp-content/uploads/2019/07/super-heros-marvel-min.jpg",
   }),
   methods: {
     async fetchData() {
       await axios
-        .get(this.url + this.ts + this.publicKey + this.hash)
+        .get(this.url + this.limit + this.offset + this.ts + this.publicKey + this.hash)
         .then((response) => {
           this.characters = response.data.data.results;
           response.data.data.results.forEach((item) => {
             this.characters.push(item);
-            console.log(item)
+            console.log(item);
           });
         });
-      console.log(this.characters)
+      console.log(this.characters);
     },
-    showInfo(index){
-      if(this.selectedIndex == index) {
-        this.selectedIndex = null
-      }else {
-        this.selectedIndex = index
+    showInfo(index) {
+      if (this.selectedIndex == index) {
+        this.selectedIndex = null;
+      } else {
+        this.selectedIndex = index;
       }
-    console.log(this.selectedIndex)
-    console.log(index)
-    }
+      console.log(this.selectedIndex);
+      console.log(index);
+    },
+    addToBookmark(hero) {
+      if (!this.bookmarkHeroes.includes(hero)) {
+        if (this.bookmarkHeroes.length > 5) {
+          this.bookmarkHeroes.splice(0, 1);
+        }
+        this.bookmarkHeroes.push(hero);
+      }
+    },
   },
   created() {
     this.fetchData();
-
   },
 };
 </script>
